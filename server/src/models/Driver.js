4d19d4ad94471b094eb476ca43333b4d85f2c29a
Harrison Onyango <alooharrison7@gmail.com>
2026-03-18 10:36:44 +0300

    Add Driver model with validation and associations for driver management

---
+++ server/src/models/Driver.js
@@ -0,0 +1,78 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const { DRIVER_STATUS } = require('../config/constants');
+
+const Driver = sequelize.define('Driver', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  first_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    validate: {
+      notEmpty: { msg: 'First name is required' },
+      len: { args: [2, 50], msg: 'First name must be 2-50 characters' },
+    },
+  },
+  last_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    validate: {
+      notEmpty: { msg: 'Last name is required' },
+      len: { args: [2, 50], msg: 'Last name must be 2-50 characters' },
+    },
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: { msg: 'Email already registered' },
+    validate: {
+      isEmail: { msg: 'Please provide a valid email' },
+    },
+  },
+  phone: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    validate: {
+      notEmpty: { msg: 'Phone number is required' },
+    },
+  },
+  license_number: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: { msg: 'License number already registered' },
+    validate: {
+      notEmpty: { msg: 'License number is required' },
+    },
+  },
+  license_expiry: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    validate: {
+      isDate: { msg: 'Please provide a valid date' },
+    },
+  },
+  vehicle_id: {
+    type: DataTypes.UUID,
+    allowNull: true,
+    references: {
+      model: 'vehicles',
+      key: 'id',
+    },
+  },
+  status: {
+    type: DataTypes.ENUM(Object.values(DRIVER_STATUS)),
+    defaultValue: DRIVER_STATUS.AVAILABLE,
+    allowNull: false,
+  },
+  profile_photo: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+}, {
+  tableName: 'drivers',
+});
+
+module.exports = Driver;
